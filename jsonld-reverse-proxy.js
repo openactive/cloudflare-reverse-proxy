4452dc774718a1f1ea16b6abc9052752ad271182
Nick Evans <2616208+nickevansuk@users.noreply.github.com>
2020-07-26 12:57:38 +0100

    Update jsonld-reverse-proxy.js

--- jsonld-reverse-proxy.js
+++ jsonld-reverse-proxy.js
@@ -1,3 +1,9 @@
+/**
+ * This Cloudflare reverse proxy serves JSON-LD for resources on the openactive.io
+ * domain when the 'Accept' header of a request contains 'application/ld+json'.
+ * This is required to ensure compliance with JSON-LD.
+ */
+
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
